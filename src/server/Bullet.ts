a8f2d8d2dadcac18a75d736dc76b570be31bb641
SemblanceOfSense <semblanceofsensegaming@gmail.com>
2024-01-08 12:48:56 -0700

    Initial Bullet Class

---
+++ src/server/Bullet.ts
@@ -0,0 +1,23 @@
+import {ServerStorage, Workspace} from "@rbxts/services";
+
+export class Bullet {
+    private position: Vector3;
+    private model: Part;
+
+    public constructor(pos: Vector3) {
+        this.position = pos;
+
+        this.model = ServerStorage.Part.Clone();
+        this.model.Parent = Workspace;
+        this.setPosition(pos);
+    }
+
+    public getPosition(): Vector3 {
+        return this.position;
+    }
+
+    public setPosition(p: Vector3): void {
+        this.position = p;
+        this.model.Position = this.position;
+    }
+}
