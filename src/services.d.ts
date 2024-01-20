d41ff55b0cf45ee631a9aed041db65ca87cc24d3
U-DESKTOP-2P4AETV\SemblanceOfSense <semblanceofsensegaming@gmail.com>
2024-01-19 17:48:29 -0700

    Moved stuff to new Place

--- src/services.d.ts
+++ src/services.d.ts
@@ -1,6 +1,32 @@
 interface Workspace extends Model {
 	Camera: Camera;
-	Baseplate: Part;
+}
+
+interface ReplicatedStorage extends Instance {
+	createServer: RemoteEvent;
+	joinServer: RemoteEvent;
+	rbxts_include: Folder & {
+		RuntimeLib: ModuleScript;
+		Promise: ModuleScript;
+		node_modules: Folder & {
+			["@rbxts"]: Folder & {
+				types: Folder & {
+					include: Folder & {
+						generated: Folder;
+					};
+				};
+				t: Folder & {
+					lib: Folder & {
+						ts: ModuleScript;
+					};
+				};
+				["compiler-types"]: Folder & {
+					types: Folder;
+				};
+				services: ModuleScript;
+			};
+		};
+	};
 }
 
 interface ServerStorage extends Instance {
